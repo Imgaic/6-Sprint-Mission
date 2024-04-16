f02e37c10ca0ee17f35eabfc27dad7b610d48c56
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-04-16 21:37:46 +0900

    Navbar 구현 완료

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import logo_mobile from "../assets/logo_mobile.png";
+import "../css/Logo.css";
+
 function Logo() {
+  const initialValue = window.innerWidth >= 744 ? logo : logo_mobile;
+  const [logoImage, setLogoImage] = useState(initialValue);
+
+  const handleImageSize = (e) => {
+    window.innerWidth >= 744 ? setLogoImage(logo) : setLogoImage(logo_mobile);
+  };
+
+  window.onresize = handleImageSize;
+
   return (
-    <>
-      <img></img>
-    </>
+    <a href="/">
+      <img className="logo__image" src={logoImage} alt="판다마켓 로고"></img>
+    </a>
   );
 }
 
