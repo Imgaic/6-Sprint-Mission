d4265e05c47069c96b47ebe7c86b6fee389ad31e
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-06-10 13:37:46 +0900

    fix: Menu style 적용 안되던것 수정, 왜 안됐는지는 모름

--- src/components/ui/Menu/index.jsx
+++ src/components/ui/Menu/index.jsx
@@ -4,10 +4,10 @@ import styles from "./styles.module.css";
 function Menu() {
   return (
     <div>
-      <menu className={styles.aligner}>
+      <ul className={styles.aligner}>
         <MenuItem link="/" name="자유게시판" />
         <MenuItem link="/items" name="중고마켓" className="active" />
-      </menu>
+      </ul>
     </div>
   );
 }
